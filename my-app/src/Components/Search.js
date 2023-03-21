b2f3e49d2a5557f19ac855926727bbd77fff2f2c
Moon ijaz <moonijaz006@gmail.com>
2023-03-21 18:45:49 +0500

    Add image to search bar file

---
+++ my-app/src/Components/Search.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Searchbar() {
+  const backgroundStyle = {
+    backgroundimage: 'url(/bimg.png)',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: '100%',
+    width: '100%',
+  };
+
+  return (
+    <div style={backgroundStyle}>
+      {/* your component content */}
+    </div>
+  );
+}
+
+export default Searchbar;
