456e784158c2814a3a31eaaac949d4521fdbdfa0
Moon ijaz <moonijaz006@gmail.com>
2023-03-24 20:48:23 +0500

    Add candidate file

---
+++ my-app/src/Components/Candidates.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+
+function Candidates() {
+  return (
+    <>
+      <SearchBar />
+      <h1>hi</h1>
+    </>
+  );
+}
+
+export default Candidates;
