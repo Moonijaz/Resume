aaf73dc08ac3b24fad4247bfa8a2a991e3ceecc4
Moon ijaz <moonijaz006@gmail.com>
2023-04-05 22:33:57 +0500

    Add packages

--- my-app/src/Components/Navbar.js
+++ my-app/src/Components/Navbar.js
@@ -4,6 +4,8 @@ import "./Navbar.css";;
 import { Link } from 'react-router-dom';
 
 
+
+
 function Navbar() {
   return (
     <body>
@@ -13,12 +15,10 @@ function Navbar() {
           <i className="fas fa-bars"></i>
         </label>
         <label className="logo">Skills</label>
-        <BrowserRouter>
         <ul>
           <li><Link to={"/Candidates"}>Candidates</Link></li>
           <li><Link to={"/Jobs"}>Jobs</Link></li>
         </ul>
-        </BrowserRouter>
       </nav>
     </body>
   );
